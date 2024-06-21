49fc39b90e6df26a33a72cc8f00e9cbdd72bffc6
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-21 20:00:30 +0500

    Variable Declarations

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # javascript-course
- 
+
+## let var const
+
+- let, const gives us scope
+
+## Questions to ponder?
+
+- What is the difference between let, const, and var?
--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello, world!");
+const accountId = 1332; // Cannot be changed
+
+let accountEmail = "a11@gmail.com";
+var accountPassword = "12345";
+accountCity = "Pasrur";
+
+
+// Same name variables in this function as above
+function A() {
+  const accountEmail = "hammad@gmail.com";
+  var accountCity = "Sialkot";
+  var accountPassword = "54321";
+  console.table([accountId, accountEmail, accountPassword, accountCity]);
+}
+
+A();
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
