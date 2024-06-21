2add3b6cd11af65246ba2fd679150aec2674834f
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-21 20:21:25 +0500

    Intro to Data Types

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # javascript-course
- 
+
+## Data Types
+
+- string
+- number
+- boolean
+- object (null)
+- undefined
+- symbol
+- bigint
--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello, world!");
+"use strict"; // treal all JS code as newer version of JS
+
+// alert("Hello"); // * Work in browsers only, not in node
+
+let name = "Hammad"; // String Data Type
+let age = 21; // Number Data Type -->  BigInt (for big numbers)
+let isLoggedIn = false; // Boolean Data Type
+let user = null; // object data type -- standalone value
+let anotherUser = undefined; // undefined data type -- Value not assigned
+let unique = Symbol("foo"); // Symbol Data Type
+
+// ? How to check data types?
+const data = {
+  name: typeof name,
+  age: typeof age,
+  isLoggedIn: typeof isLoggedIn,
+  user: typeof user,
+  anotherUser: typeof anotherUser,
+  unique: typeof unique,
+}
+console.table(data);
+
