ce653143646804a23ade4180c9f9d8a60211a26c
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-21 21:38:14 +0500

    Strings

---
+++ strings.js
@@ -0,0 +1,53 @@
+const demoName = new String("Hammad");
+console.log(demoName);
+
+// ! Concatenate
+// const name = "Hammad";
+// const repoCount = 20;
+// console.log(name + repoCount);
+// console.log(`${name} has ${repoCount} repositories`); // Backticks
+
+// ! Access Characters and their position in String
+// const name = new String("Hammad");
+// console.log(name[0]); // H
+// console.log(name.charAt(5)); // d
+// console.log(name.indexOf("m")); // 2
+// console.log(name.__proto__); // {}
+
+// ! Get the length of string
+// const name = "Hammad";
+// console.log(name.length); // 6
+
+// ! Change cases of String
+// const name = "Hammad";
+// console.log(name.toLowerCase());
+// console.log(name.toUpperCase());
+
+// ! Divide Strings
+// const name = "Hammad";
+// const subName1 = name.substring(0, 3); // Ham
+// const subName2 = name.slice(0, 3); // Ham
+// const subName3 = name.slice(-4, 3); // m
+// console.log(subName1, subName2, subName3);
+
+// ! Remove spaces
+// const name = "      Hammad      ";
+// const trimmedName = name.trim();
+// console.log(name);  // "      Hammad      "
+// console.log(trimmedName);  // "Hammad"
+
+// ! Replace characters or spaces in strings
+// const url = "https://hammad.com/hammad afzal";
+// const modifiedUrl = url.replace(" ", "-");
+// console.log(url); // https://hammad.com/hammad afzal
+// console.log(modifiedUrl); // https://hammad.com/hammad-afzal
+
+// ! Check inclusion of characters in strings
+// const url = "https://hammad.com/hammad afzal";
+// console.log(url.includes("h"));
+
+// ! Converts Strings to Array
+// const name = "Muhammad-Hammad-Afzal";
+// const nameArray = name.split("-");
+// console.log(name); // Muhammad-Hammad-Afzal
+// console.log(nameArray); // [Muhammad, Hammad, Afzal]
