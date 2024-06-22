93f037fe5169c7e2b2165115143b02fc837ab024
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-22 14:14:35 +0500

    Defined Logic by declaring functions

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hello, world!");
+const randomNum = parseInt(Math.random() * 100 + 1);
+
+const submitBtn = document.querySelector("#c");
+const userInput = document.querySelector("#guessField");
+const guesses = document.querySelector(".guesses");
+const guessRemaining = document.querySelector(".lastResult");
+const lowOrHigh = document.querySelector(".lowOrHi");
+const startOver = document.querySelector(".resultParas");
+
+const p = document.createElement("p");
+
+let prevGuesses = [];
+let numGuesses = 1;
+let playGame = true;
+
+function validateGuess(guess) { }
+function checkGuess(guess) { }
+function displayMessage(message) { }
+function displayGuess() { }
+function newGame() { }
+function endGame() { }
