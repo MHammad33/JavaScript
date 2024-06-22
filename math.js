4db6c3d9aab109289afd551f98c6f2c8c24651fc
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-22 22:02:02 +0500

    Why Math.PI value can never be changed in JS?

---
+++ math.js
@@ -0,0 +1,17 @@
+// console.log(Math.PI);
+// Math.PI = 5;
+// console.log(Math.PI);
+
+// ! REMEMEBER
+// const descriptor = Object.getOwnPropertyDescriptor(Math, "PI");
+// console.log(descriptor);
+
+// const user = { name: "Hammad", age: 21, isStudent: true };
+// const descriptor = Object.getOwnPropertyDescriptor(user, "name");
+// console.log(descriptor);
+
+// Object.defineProperty(user, "name", {
+//   writable: false
+// });
+// user.name = "Ali"; // Cannot override
+// console.log(user);
