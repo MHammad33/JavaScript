b1c6dbbbfeb38286176cb96ae55c63fc01c763a7
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-22 20:08:15 +0500

    Object Literals

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello, world!");
+// ! Classes and Object Oriented (OOP)
+
+// ? Object Literals
+// const user = {
+//   username: "Hammad",
+//   loginCount: 8,
+//   signedIn: true,
+
+//   getUserDetails: function () {
+//     console.log(`Got user details from database: ${this.username}`)
+//     console.log(this); // ? this -> whole object reference
+//   }
+// };
+
+// console.log(user.username);
+// console.log(user.getUserDetails());
+
