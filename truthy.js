a42b6674f4a60a9f92b3a9c38d2163c70806c06c
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-22 09:04:16 +0500

    Truthy and Falsy Values

---
+++ truthy.js
@@ -0,0 +1,14 @@
+// ! Truthy and Falsy Values
+
+// const userEmail = "hammad@gmail.com";
+// const userEmail = "";
+
+// if (userEmail) {
+//   console.log("Email Exists");
+// } else {
+//   console.log("No Email");
+// }
+
+// * Notes
+// ? "", 0, -0, 0n, null, undefined => falsy values
+// ? "0", "false", " ", [], {}, function(){} => truthy values
