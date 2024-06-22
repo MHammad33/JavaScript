86efac962a6b12b1a53187eba9c10b54829c7447
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-22 20:20:49 +0500

    Constructor Functionslo

--- index.js
+++ index.js
@@ -15,3 +15,33 @@
 // console.log(user.username);
 // console.log(user.getUserDetails());
 
+// ? Constructor Function -- new keyword
+// function User(username, loginCount, isLoggedIn) {
+//   this.username = username;
+//   this.loginCount = loginCount;
+//   this.isLoggedIn = isLoggedIn;
+
+//   this.greeting = function () {
+//     console.log(`Welcome ${this.username}`);
+//   }
+
+//   return this;
+// }
+
+// const user = new User("Hammad", 12, true);
+// console.log(user.constructor);
+
+
+// * Without using Constructor
+// const user1 = User("Hammad", 7, true);
+// const user2 = User("Ali", 17, false);
+// console.log(user1.username); // Ali
+// console.log(user2.username); // Ali
+
+// * Using Constructor
+// const user1 = new User("Hammad", 7, true);
+// const user2 = new User("Ali", 17, false);
+// console.log(user1.username); // Hammad
+// console.log(user2.username); // Ali
+
+
