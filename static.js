aefe4ae9f09aa83553fe0e64ee885d964a4ad884
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-22 21:38:55 +0500

    Static Methods

---
+++ static.js
@@ -0,0 +1,26 @@
+// ! Static
+
+// class User {
+//   constructor(username) {
+//     this.username = username
+//   }
+
+//   logMe() {
+//     console.log(`USERNAME: ${this.username}`);
+//   }
+
+//   static createId() {
+//     return `123`;
+//   }
+// }
+
+// const user = new User("Hammad");
+// // console.log(user.createId()); // error
+// console.log(User.createId()); // 123
+
+// class Teacher extends User { }
+
+
+// const teacher = new Teacher();
+// // console.log(teacher.createId()); // error
+// console.log(Teacher.createId()); // 123
