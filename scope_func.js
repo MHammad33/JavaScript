803bd1142ab95519637433b65c440b78561553d9
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-22 06:50:38 +0500

    Scope Level and Mini Hoisting

--- scope_func.js
+++ scope_func.js
@@ -15,3 +15,33 @@
 // console.log(d); // 30
 
 // TODO: Find out difference between global object in node and browser
+
+// ? Scope in Functions
+// function one() {
+//   const username = "Hammad";
+//   function two() {
+//     const website = "Youtube";
+//     console.log(username);
+//   }
+//   // console.log(website); // Error -- scope issue
+//   two();
+// }
+// one();
+
+// ? Line by Line execution of errors
+// if (true) {
+//   const username = "Hammad";
+//   if (username === "Hammad") {
+//     const website = "YouTube";
+//     console.log(username, website); // Hammad YouTube
+//   }
+//   console.log(website); // Error
+// }
+// console.log(username); // Could Not reach this line due to error
+
+// ? Mini Hoisting
+// console.log(addOne(3)); // 4
+// function addOne(num) { return num + 1 };
+
+// console.log(addTwo(3)); // Error
+// const addTwo = function (num) { return num + 2 }
