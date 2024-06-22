2407f6edcc4e8a830e92c58f3853b5e1fbc9271d
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-22 09:26:00 +0500

    For Loop

---
+++ loops.js
@@ -0,0 +1,39 @@
+// ! Loops
+
+// * For Loop, While Loop, Do-While Loop
+
+// ? For Loop
+// for (let i = 0; i < 5; i++) {
+//   console.log(i); // 0, 1, 2, 3, 4
+// }
+
+// ? Nested Loops
+// for (let i = 0; i < 5; i++) {
+//   console.log("OUTER LOOP");
+//   for (let j = 0; j < 5; j++) {
+//     console.log("INNER LOOP", j);
+//   }
+// }
+
+// ? Looping through array
+// const arr = [1, 2, 3, 4, 5];
+// for (let i = 0; i < arr.length; i++) {
+//   const element = arr[i];
+//   console.log(element); // 1, 2, 3, 4, 5
+// }
+
+// ? Break statement in For Loop
+// for (let i = 0; i < 5; i++) {
+//   if (i == 3) {
+//     break;
+//   }
+//   console.log(i); // 0, 1, 2
+// }
+
+// ? Continue statement in For Loop
+// for (let i = 0; i < 5; i++) {
+//   if (i == 3) {
+//     continue;
+//   }
+//   console.log(i); // 0, 1, 2, 4
+// }
