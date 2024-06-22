41728eb67f4d58a26bf34a78153fc479f97536c5
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-22 17:28:06 +0500

    Completed Project

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Hello, world!");
+// Generate a Random Color
+const randomColor = () => {
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    const randomValue = Math.floor(Math.random() * 16);
+    color += hex[randomValue];
+  }
+  return color;
+}
+
+document.querySelector("#start").addEventListener("click", startChangingColor); document.querySelector("#stop").addEventListener("click", stopChangingColor);
+
+let intervalId;
+function startChangingColor() {
+  if (!intervalId) {
+    console.log("Started Changing Color");
+    intervalId = setInterval(() => {
+      document.body.style.backgroundColor = randomColor();
+    }, 1000);
+  } else {
+    console.log("Already Changing Color");
+  }
+
+}
+function stopChangingColor() {
+  console.log("Stopped Changing Color");
+  clearInterval(intervalId);
+  intervalId = null;
+}
