b3bcfdfd69739419b9c01f2284cb544f2ff3aff4
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-22 06:13:11 +0500

    Functions and Parameters

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log("Hello, world!");
+// ! Functions
+
+// function defFun() { }
+// defFun // reference
+// defFun() // calling
+
+// ? Parameters -- Add 2 numbers
+// function addTwoNumbers(num1, num2) {
+//   console.log(num1 + num2);
+// }
+
+// ? Arguments -- When Calling function
+// addTwoNumbers(); // NaN
+// addTwoNumbers(1); // NaN
+// addTwoNumbers(1, 1); // 2
+// addTwoNumbers(1, "1"); // 11
+// addTwoNumbers(1, "a"); // 1a
+// addTwoNumbers(1, null); // 1
+// addTwoNumbers(1, 1, 1); // 2
+
+// ? Return Values
+// function addTwoNumbers(num1, num2) {
+//   return num1 + num2;
+// }
+// console.log("Result:", addTwoNumbers(3, 5)); // 8
+
+// ? If no argument passed, but parameter is used
+// function loginUserMessage(username) {
+//   return `${username} just logged in`;
+// }
+// console.log(loginUserMessage("Hammad")); // Hammad just logged in
+// console.log(loginUserMessage()); // undefined just logged in
+
+// ? Default values in parameters
+// function loginUserMessage(username = "Someone") {
+//   return `${username} just logged in`;
+// }
+// console.log(loginUserMessage("Hammad")); // Hammad just logged in
+// console.log(loginUserMessage()); // Someone just logged in
