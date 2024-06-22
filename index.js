50c10cef23e9f8228385694e9dbfa8f1a036db91
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-22 17:35:36 +0500

    Complete Project

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello, world!");
+const insert = document.getElementById("insert");
+
+window.addEventListener("keydown", (e) => {
+  insert.innerHTML = `<div clss="color">
+  <table>
+    <tr>
+      <th>Key</th>
+      <th>KeyCode</th>
+      <th>Code</th>
+    </tr>
+    <tr>
+      <td>${e.key === " " ? "Space" : e.key}</td>
+      <td>${e.keyCode}</td>
+      <td>${e.code}</td>
+    </tr>
+  </table>
+  </div>`
+})
