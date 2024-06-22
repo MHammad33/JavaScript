7836daa13e3c8560620ba59953e86667af255446
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-22 06:26:39 +0500

    Global and Local Scope

---
+++ scope_func.js
@@ -0,0 +1,17 @@
+// ! Scopes in Functions
+
+// var d = 300;
+
+// if (true) {
+//   let a = 10;
+//   const b = 20;
+//   var c = 30;
+//   var d = 30; // Override
+// }
+
+// console.log(a); // error a is not defined
+// console.log(b); // error b is not defined
+// console.log(c); // 30
+// console.log(d); // 30
+
+// TODO: Find out difference between global object in node and browser
