56e211da8cdaaf7ac1208d1c136713a081d19bf4
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-22 13:50:35 +0500

    Project Completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello, world!");
+const clock = document.getElementById("clock");
+
+setInterval(() => {
+  const date = new Date();
+  const localTime = date.toLocaleTimeString();
+  clock.innerHTML = localTime
+}, 1000);
