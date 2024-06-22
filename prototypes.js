3e092f0c3465d39ce3561caa823893634c73d924
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-22 20:52:06 +0500

    Prototypes

---
+++ prototypes.js
@@ -0,0 +1,37 @@
+// ! Prototypes
+
+// const name = "Hammad      ";
+// console.log(name.length); // 12
+// console.log(name.trim().length); // 6
+
+// ? Create Object Prototypes
+// let myHeroes = ["Hero1", "Hero2"];
+// let heroPower = {
+//   hero1: "hammer",
+//   hero2: "sling",
+//   getHero2Power: function () {
+//     console.log(`Hero2 Power is ${this.hero2}`);
+//   }
+// }
+// Object.prototype.Hammad = function () {
+//   console.log("Hammad is present in all objects");
+// }
+// heroPower.Hammad();
+
+// ? Create Prototypes for all objects
+// const obj = {};
+// const arr = [];
+
+// Object.prototype.Hammad = function () {
+//   console.log("Hammad is present in all objects");
+// }
+
+// Array.prototype.Ali = function () {
+//   console.log("Ali is present in only arrays");
+// }
+
+// obj.Hammad();
+// // obj.Ali(); // error
+
+// arr.Hammad();
+// arr.Ali();
