9b482364567d42c631a2a3418f9aea51eba78340
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-23 08:22:57 +0500

    Lexical Scoping

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("Hello, world!");
+// ! Lexical Scoping and Closures
+
+// ? Scope
+// function outer() {
+//   let username = "Hammad";
+// }
+// console.log(username); // error
+
+
+// ? Lexical Scoping (inner functions have access to outer function)
+// function init() {
+//   let name = "Mozilla"; // name is a local variable created by init
+//   function displayName() {
+//     // displayName() is the inner function, that forms the closure
+//     console.log(name); // use variable declared in the parent function
+//   }
+//   displayName();
+// }
+// init();
+
+// * Another Example
+// function outer() {
+//   let username = "Hammad";
+//   // console.log("secretInnerOne", secretInnerOne); // error
+//   function innerOne() {
+//     let secretInnerOne = "secretInnerOne";
+//     console.log("Inner One", username);
+//   }
+
+//   function innerTwo() {
+//     console.log("Inner Two", username);
+//   }
+
+//   innerOne();
+//   innerTwo();
+// }
+// outer();
+
