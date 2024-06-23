9ea8ed48af82048ff8ae9b99c5a7e2cc2c6e96dc
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-23 08:26:43 +0500

    Closures Basics

--- index.js
+++ index.js
@@ -36,3 +36,14 @@
 // }
 // outer();
 
+// ? Closures
+// function makeFunc() {
+//   const name = "Mozilla";
+//   function displayName() {
+//     console.log(name);
+//   }
+//   return displayName;
+// }
+
+// const myFunc = makeFunc();
+// myFunc();
